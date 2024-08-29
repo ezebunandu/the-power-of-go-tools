95ca142431f6cdf585d3c9f88ca6a83e5a26b191
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2024-08-28 23:25:37 -0600

    using a struct type to set default location to print

--- hello/cmd/hello/main.go
+++ hello/cmd/hello/main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
 
 	"github.com/ezebunandu/hello"
 )
 
 func main() {
-	fmt.Println("Listening on http://localhost:8222")
-	http.ListenAndServe(":8222", http.HandlerFunc(
-		func(w http.ResponseWriter, r *http.Request){
-			hello.PrintTo(w)
-		}),
-	)
+	p := hello.NewPrinter()
+	p.Print()
 }
--- hello/hello.go
+++ hello/hello.go
@@ -3,8 +3,19 @@ package hello
 import (
 	"fmt"
 	"io"
+	"os"
 )
 
-func PrintTo(w io.Writer){
-	fmt.Fprintln(w, "Hello, world")
+type Printer struct {
+	Output io.Writer
+}
+
+func NewPrinter() *Printer {
+	return &Printer{
+		Output: os.Stdout,
+	}
+}
+
+func (p *Printer) Print() {
+	fmt.Fprintln(p.Output, "Hello, world")
 }
