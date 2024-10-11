81968986b8f654e64d01c9b99d521b9e46f84979
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2024-10-11 06:58:22 -0600

    a test for match.PrintMatchingLines?

--- match/match_test.go
+++ match/match_test.go
@@ -1,12 +1,35 @@
 package match_test
 
 import (
+	"bytes"
+	"fmt"
+	"os"
 	"testing"
-    "os"
 
 	"github.com/ezebunandu/match"
 	"github.com/rogpeppe/go-internal/testscript"
 )
+
+func TestMatch_PrintsMatchingLinesToGivenWriter(t *testing.T){
+	t.Parallel()
+    args, err := os.Open("testdata/three_lines.txt") 
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+	buf := new(bytes.Buffer)
+	m, err := match.NewMatcher(match.WithInput(args), match.WithOutput(buf), match.WithSearchStringFromArgs([]string{"DUST"}))
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+	m.PrintMatchingLines()
+	want := "are sprinkled with magic DUST\na special kind of DUST\nDUST like no other"
+	got := buf.String()
+	if want != got {
+		t.Errorf("want %q, got %q", want, got)
+	}
+}
 
 func Test(t *testing.T){
     t.Parallel()
